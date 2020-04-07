ebbfe031607c63ab90c11d9121c3fdc9170aa564
okarinbro <prz.jab98@gmail.com>
2020-04-07 18:58:46 +0200

    Create classifiers test and filter invalid features out

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 
@@ -5,7 +6,6 @@ def read_data(path, target):
     data = pd.read_csv(path)
     df = pd.DataFrame(data=data)
     labels = df[target].values
-    features = list(filter(lambda x: x not in {target, 'INITIALS'}, df.columns))
-    d = df[features]
-    d = d.dropna(axis=1)
-    return d, labels
+    del df[target]
+    df = df[df.select_dtypes([np.number]).columns].dropna(axis=1)
+    return df, labels
--- main.py
+++ main.py
@@ -1,19 +1,43 @@
 import subprocess
 
 from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score, recall_score
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
 
 from data import read_data
-from importance_plot import show_importance
+from feature_util import get_best_k_features
+
+
+def test_classifiers(data, labels):
+    train_features, test_features, train_labels, test_labels = train_test_split(data, labels,
+                                                                                test_size=0.2,
+                                                                                random_state=42)
+    svm = SVC()
+    nn = MLPClassifier()
+    rf = RandomForestClassifier()
+    classifiers = {"svm": svm, "nn": nn, "rf": rf}
+    cls_params = {"svm": {'kernel': ('linear', 'rbf'), 'C': [1, 5, 10]},
+                  "nn": {'activation': ['relu', 'tanh', 'logistic'],
+                         'hidden_layer_sizes': [(100,), (10, 50), (50, 100)],
+                         'solver': ['sgd', 'adam'], 'learning_rate': ['adaptive']},
+                  "rf": {'n_estimators': [50, 100, 200, 300]}}
+
+    for k, v in classifiers.items():
+        cv = GridSearchCV(v, cls_params[k])
+        cv.fit(train_features, train_labels)
+        pred = cv.predict(test_features)
+        print(k)
+        print('f1: ', f1_score(test_labels, pred, average="macro"))
+        print('recall: ', recall_score(test_labels, pred, average="macro"))
+        print('accuracy: ', accuracy_score(test_labels, pred))
+
 
 if __name__ == '__main__':
-    # print("Select algorithm: [mcfs, rf]")
-    # algo = input()
-    # print("Select training data path: ")
-    # path = input()
-    # print("Type target label")
-    # target = input()
-    algo = 'mcfs'
+    algo = 'rf'
     path = 'data.csv'
     target = 'SEX'
     k = 20
@@ -24,7 +48,10 @@ if __name__ == '__main__':
                                                                                     test_size=0.2,
                                                                                     random_state=42)
         forest.fit(train_features, train_labels)
-        show_importance(forest, train_features, k)
+        first_k_features = get_best_k_features(forest, train_features, k)
+
+        test_classifiers(data[first_k_features], labels)
+
     elif algo == "mcfs":
         subprocess.run(["Rscript", "sex_classifier.R"])
     else:
