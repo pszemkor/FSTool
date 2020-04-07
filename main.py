c45a69810f5a17d18966ca33b41e81f398ab0208
filipsl <filip.slazyk@gmail.com>
2020-04-07 19:19:17 +0200

    Add pandas dataframe conversion

--- main.py
+++ main.py
@@ -11,6 +11,15 @@ from sklearn.svm import SVC
 from data import read_data
 from feature_util import get_best_k_features
 
+from rpy2.robjects import r
+import pandas as pd
+
+import rpy2.robjects as ro
+from rpy2.robjects.packages import importr
+from rpy2.robjects import pandas2ri
+
+from rpy2.robjects.conversion import localconverter
+
 
 def test_classifiers(data, labels):
     train_features, test_features, train_labels, test_labels = train_test_split(data, labels,
@@ -36,11 +45,8 @@ def test_classifiers(data, labels):
         print('accuracy: ', accuracy_score(test_labels, pred))
 
 
-from rpy2.robjects import r
-
-
 if __name__ == '__main__':
-    algo = 'rf'
+    algo = 'mcfs'
     path = 'data.csv'
     target = 'SEX'
     k = 20
@@ -69,7 +75,12 @@ if __name__ == '__main__':
         result = r['result']
         ri = r['RI']
         print('RI',ri)
+        print(dir(ri), type(ri))
 
+        with localconverter(ro.default_converter + pandas2ri.converter):
+          pd_df = ro.conversion.rpy2py(ri)
+
+        print(pd_df)
 
     else:
         raise Exception("Unknown algorithm")
