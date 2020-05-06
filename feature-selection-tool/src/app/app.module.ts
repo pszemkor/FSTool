1e1f72c36cecb7b8250437d6cf75f5f9b195e675
filipsl <filip.slazyk@gmail.com>
2020-05-06 23:58:49 +0200

    Add parameters form.

--- feature-selection-tool/src/app/app.module.ts
+++ feature-selection-tool/src/app/app.module.ts
@@ -1,17 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing/app-routing.module'
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing/app-routing.module'
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { MainComponent } from './main/main.component';
-import { HeaderComponent } from './header/header.component';
+
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {MainComponent} from './main/main.component';
+import {HeaderComponent} from './header/header.component';
+import { ParamsformComponent } from './paramsform/paramsform.component';
 
 
 @NgModule({
@@ -19,7 +26,8 @@ import { HeaderComponent } from './header/header.component';
     AppComponent,
     HomeComponent,
     MainComponent,
-    HeaderComponent
+    HeaderComponent,
+    ParamsformComponent
   ],
   imports: [
     BrowserModule,
@@ -30,8 +38,14 @@ import { HeaderComponent } from './header/header.component';
     MatIconModule,
     FlexLayoutModule,
     MatSidenavModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
