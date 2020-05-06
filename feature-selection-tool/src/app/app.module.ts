7c7939a1afb7e2ff4834bfeb69b65eae40d1142c
Przemysław Jabłecki <prz.jab98@gmail.com>
2020-05-06 12:30:14 +0200

    Change style of header

--- feature-selection-tool/src/app/app.module.ts
+++ feature-selection-tool/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing/app-routing.module'
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -22,6 +26,10 @@ import { HeaderComponent } from './header/header.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    FontAwesomeModule,
+    MatIconModule,
+    FlexLayoutModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
