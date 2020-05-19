c30b121a6593b7ec15e6f663975246769e347239
Przemysław Jabłecki <prz.jab98@gmail.com>
2020-05-19 21:14:02 +0200

    Update views

--- backend/fst_server/views.py
+++ backend/fst_server/views.py
@@ -6,13 +6,11 @@ sys.path.append(os.path.abspath('../'))
 sys.path.append(os.path.abspath('..'))
 from execution.CommandExecutor import CommandExecutor
 
-
 @api_view(['GET', 'POST'])
 def fs_request(request):
     if request.method == "POST":
         print(request.data)
         executor = CommandExecutor(request.data)
-
         execute = executor.execute()
         json = execute.toJSON()
         print(json)
