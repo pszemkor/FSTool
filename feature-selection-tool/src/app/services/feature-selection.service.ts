09f9362d333a0a54a4a0faba4eedd1960b27aa75
Przemysław Jabłecki <prz.jab98@gmail.com>
2020-05-19 21:21:29 +0200

    Catch error in feature selection service

--- feature-selection-tool/src/app/services/feature-selection.service.ts
+++ feature-selection-tool/src/app/services/feature-selection.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { baseURL } from '../shared/baseurl';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { FeatureSelectionParameters } from '../shared/featureselectionparameters'
-import {FeatureSelectionResults} from '../shared/featureselectionresults'
+import { FeatureSelectionParameters } from '../shared/featureselectionparameters';
+import { FeatureSelectionResults } from '../shared/featureselectionresults';
+import { ErrorMessageProcessorService } from './error-message-processor.service';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeatureSelectionService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private errorProcessor: ErrorMessageProcessorService) { }
 
   postRequest(params: FeatureSelectionParameters) {
     const httpOptions = {
@@ -18,7 +21,8 @@ export class FeatureSelectionService {
       })
     };
     console.log(baseURL + 'featureselection')
-    return this.http.post<FeatureSelectionResults>(baseURL + 'featureselection', params, httpOptions);
+    return this.http.post<FeatureSelectionResults>(baseURL + 'featureselection', params, httpOptions)
+      .pipe(catchError(this.errorProcessor.handleError));
   }
 }
 
