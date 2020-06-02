4859e71196ec7e23a048ffd9ea0d50f15e937646
filipsl <filip.slazyk@gmail.com>
2020-06-02 16:41:38 +0200

    Read data for evaluation

--- backend/fst_server/views.py
+++ backend/fst_server/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, JsonResponse
 from rest_framework.decorators import api_view
 
 from fst_server.models import Classifier
+from execution.CommandExecutor import CommandExecutor, ModelEvaluator
 
 sys.path.append(os.path.abspath('../'))
 sys.path.append(os.path.abspath('..'))
@@ -32,3 +33,13 @@ def get_models(request):
             }
             models.append(clf_without_blob)
     return JsonResponse({'models': models})
+
+
+@api_view(['GET', 'POST'])
+def clf_request(request):
+    if request.method == "POST":
+        print(request.data)
+        evaluator = ModelEvaluator(request.data)
+        evaluation_data = {}
+        # evaluation_data = evaluator.execute()
+        return JsonResponse(evaluation_data)
