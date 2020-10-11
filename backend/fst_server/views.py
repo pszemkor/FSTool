92abaaa932c4492911b5946c3d1bf4e5b3d99a84
okarinbro <prz.jab98@gmail.com>
2020-10-11 14:31:09 +0200

    Implement job submission endpoint

--- backend/fst_server/views.py
+++ backend/fst_server/views.py
@@ -8,17 +8,28 @@ from django.forms.models import model_to_dict
 sys.path.append(os.path.abspath('../'))
 sys.path.append(os.path.abspath('..'))
 from execution.CommandExecutor import CommandExecutor
+import json
+import requests
 
 
 @api_view(['GET', 'POST'])
 def fs_request(request):
-    if request.method == "POST":
-        print(request.data)
-        executor = CommandExecutor(request.data)
-        execute = executor.execute()
-        json = execute.toJSON()
-        print(json)
-        return HttpResponse(json)
+    if request.method == 'POST':
+        # Rimrock's hello world =]
+        settings = HPCSettings.objects.all()
+        if len(settings) != 1:
+            return HttpResponse("Missing settings", status=500)
+        user_settings = settings[0]
+        sample_data = {'host': user_settings.host,
+                       'script': '#!/bin/bash\n#SBATCH -A plgimmunome\n \echo hello\nexit 0'}
+        header = {'Content-type': 'application/json', "PROXY": user_settings.proxy_certificate}
+        response = requests.post('https://rimrock.plgrid.pl/api/jobs', json=sample_data, headers=header)
+        response_content = response.json()
+        if response.ok:
+            Job.objects.create(job_id=response_content['job_id'], status=response_content['status'])
+        else:
+            return HttpResponse(response.reason, status=500)
+        return HttpResponse(response.text)
 
 
 @api_view(['GET', 'POST'])
@@ -69,4 +80,5 @@ def jobs(request):
     status = request.data['status']
     start_time = request.data['start_time']
     end_time = request.data['end_time']
-    return JsonResponse(model_to_dict(Job.objects.create(job_id=job_id, status=status, start_time=start_time, end_time=end_time)))
+    return JsonResponse(
+        model_to_dict(Job.objects.create(job_id=job_id, status=status, start_time=start_time, end_time=end_time)))
