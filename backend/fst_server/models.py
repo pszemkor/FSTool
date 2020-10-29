871f0a8e78a89a79112c0f92505919435d02e315
Przemysław Jabłecki <prz.jab98@gmail.com>
2020-10-29 15:59:56 +0100

    Use FSResult instead of JobResult

--- backend/fst_server/models.py
+++ backend/fst_server/models.py
@@ -1,5 +1,3 @@
-import json
-
 from django.db import models
 
 
@@ -12,17 +10,6 @@ def auto_str(cls):
 
     cls.__str__ = __str__
     return cls
-
-
-class Classifier(models.Model):
-    name = models.CharField(max_length=100)
-    details = models.CharField(max_length=300, default="")
-    creation_timestamp = models.DateTimeField(auto_now_add=True)
-    cls_pickle = models.BinaryField()
-    selected_features = models.TextField(null=False)
-
-    def __str__(self):
-        return self.name
 
 
 @auto_str
@@ -40,11 +27,24 @@ class Job(models.Model):
     end_time = models.CharField(max_length=100, null=False)
 
 
-class JobResult(models.Model):
+@auto_str
+class FSResult(models.Model):
     job_id = models.CharField(primary_key=True, max_length=200, null=False)
     response_json = models.TextField(null=False)
 
 
+@auto_str
 class Image(models.Model):
     image_binary = models.BinaryField(null=False)
-    job_result = models.ForeignKey(JobResult, on_delete=models.CASCADE)
+    fs_result = models.ForeignKey(FSResult, on_delete=models.CASCADE)
+
+
+class Classifier(models.Model):
+    name = models.CharField(max_length=100)
+    details = models.CharField(max_length=300, default="")
+    creation_timestamp = models.DateTimeField(auto_now_add=True)
+    cls_pickle = models.BinaryField()
+    selected_features = models.TextField(null=False)
+
+    def __str__(self):
+        return self.name
