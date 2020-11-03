f0c039ead4f33cf60284a72d77964958539b0956
Przemysław Jabłecki <prz.jab98@gmail.com>
2020-11-03 15:47:23 +0100

    Remove incorrect import

--- feature-selection-tool/src/app/app.module.ts
+++ feature-selection-tool/src/app/app.module.ts
@@ -1,40 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing/app-routing.module'
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { HttpClientModule } from '@angular/common/http';
-import { MatListModule } from '@angular/material/list';
-import { FeatureSelectionService } from './services/feature-selection.service';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing/app-routing.module';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {HttpClientModule} from '@angular/common/http';
+import {MatListModule} from '@angular/material/list';
+import {FeatureSelectionService} from './services/feature-selection.service';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { MainComponent } from './main/main.component';
-import { HeaderComponent } from './header/header.component';
-import { HighlightDirective } from './directives/highlight.directive';
-import { ParamsformComponent } from './paramsform/paramsform.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {MainComponent} from './main/main.component';
+import {HeaderComponent} from './header/header.component';
+import {HighlightDirective} from './directives/highlight.directive';
+import {ParamsformComponent} from './paramsform/paramsform.component';
 
-import { baseURL } from './shared/baseurl';
-import { ResultsComponent } from './results/results.component'
-import { MatTableModule } from '@angular/material/table';
-import { ModelsComponent } from './models/models.component';
-import { ModelsformComponent } from './modelsform/modelsform.component';
-import { ClassificationresultComponent } from './classificationresult/classificationresult.component';
-import { HPCSettingsComponent } from './hpc-settings/hpc-settings.component';
-import { JobsComponent } from './jobs/jobs.component';
+import {baseURL} from './shared/baseurl';
+import {ResultsComponent} from './results/results.component';
+import {MatTableModule} from '@angular/material/table';
+import {ModelsComponent} from './models/models.component';
+import {ModelsformComponent} from './modelsform/modelsform.component';
+import {HPCSettingsComponent} from './hpc-settings/hpc-settings.component';
+import {JobsComponent} from './jobs/jobs.component';
 import {MatGridListModule} from '@angular/material/grid-list';
-import { HPCSettingsFormComponent } from './hpc-settings-form/hpc-settings-form.component';
+import {HPCSettingsFormComponent} from './hpc-settings-form/hpc-settings-form.component';
 import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
@@ -47,7 +47,6 @@ import {MatInputModule} from '@angular/material/input';
     ResultsComponent,
     ModelsComponent,
     ModelsformComponent,
-    ClassificationresultComponent,
     HPCSettingsComponent,
     JobsComponent,
     HPCSettingsFormComponent
